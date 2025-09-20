31ce422afd83b486953384753e466a17e79e705b
dimonchikous <dima.maczkovskij3010@gmail.com>
2025-09-20 19:28:52 +1000

    fixed buffer bug

--- rcall.c
+++ rcall.c
@@ -5,7 +5,7 @@ bool rcall_issaved[10]={0};
 int rcall_saved[10];
 
 void remember(int a, int slot){
-	if (slot >= 0 && slot <= 10){
+	if (slot >= 0 && slot < 10){
 		rcall_saved[slot] = a;
 		rcall_issaved[slot] = 1;
 	}
@@ -14,7 +14,7 @@ void remember(int a, int slot){
 }
 
 int recall(int slot){
-	if (slot >= 0 && slot <= 10){
+	if (slot >= 0 && slot < 10){
 		if (rcall_issaved[slot]==0){
 			printf("Error:Value not saved! -1 was recalled\n");
 			return -1;
@@ -34,7 +34,7 @@ int recall(int slot){
 }
 
 void cleansave(int slot){
-	if (slot >= 0 && slot <= 10){
+	if (slot >= 0 && slot < 10){
 		rcall_saved[slot] = 0;
 		rcall_issaved[slot] = 0;
 	}
